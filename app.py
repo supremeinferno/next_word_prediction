d4f9a82168e4f046b9d6d3d0a66a1ddd6566841b
Pranav Garg <317madhavgarg@gmail.com>
2026-06-14 18:30:12 +0530

    adding requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import numpy as np
 import pickle
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 st.set_page_config(
     page_title="WordFlow · Next Word AI",
